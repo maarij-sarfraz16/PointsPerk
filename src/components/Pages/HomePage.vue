69c7ad1f3ae807b571e6b00caf3f2079db46ffdc
Maarij Sarfraz <maarijsarfraz13@gmail.com>
2024-03-12 22:37:02 +0500

    redemption points and csv

--- src/components/Pages/HomePage.vue
+++ src/components/Pages/HomePage.vue
@@ -4,17 +4,9 @@
 
   <v-main>
     <SideBar :drawer="drawer" />
-
     <v-container>
-      <v-row justify="start">
-
-        <v-col cols="12" sm="6" md="4" lg="3">
-          <v-card width="400" title="Redemption Points" text="" variant="elevated" color="#F37973"></v-card>
-        </v-col>
-
-      </v-row>
-    </v-container>
-    
+       <redemptionPoints/> 
+      </v-container>
   </v-main>
 </template>
 
@@ -22,6 +14,7 @@
 
   import NavBar from "../Global/NavBar.vue";
   import SideBar from "../Global/SideBar.vue";
+  import redemptionPoints from "./userDashBoard/redemptionPoints.vue";
 
   export default {
     
@@ -29,6 +22,8 @@
     components: {
       NavBar,
       SideBar,
+      redemptionPoints,
+     
     },
 
     data() {
