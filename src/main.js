8b2dd5197cb27a6e8c351c1280e3b7cba666c8fa
Wahab3917 <wahab3917@gmail.com>
2024-03-15 04:23:50 +0500

    Refactoring

--- src/main.js
+++ src/main.js
@@ -4,16 +4,23 @@ import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
 import { createRouter, createWebHistory } from "vue-router"; // Import createRouter and createWebHistory
 
-import HomePage from "./components/Pages/HomePage.vue";
-import LoginPage from "./components/Pages/LoginPage.vue";
+import userDashboard from "./components//User/userDashboard.vue";
+import adminDashboard from "./components/Admin/adminDashboard.vue";
+import userLogin from "./components/User/userLogin.vue";
+import userSignup from "./components/User/userSignup.vue";
+import adminLogin from "./components/Admin/adminLogin.vue";
 
 loadFonts();
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: "/", component: HomePage },
-    { path: "/login", component: LoginPage },
+    { path: "/user", component: userDashboard },
+    { path: "/admin", component: adminDashboard },
+
+    { path: "/login", component: userLogin },
+    { path: "/signup", component: userSignup },
+    { path: "/admin-login", component: adminLogin },
   ],
 });
 
