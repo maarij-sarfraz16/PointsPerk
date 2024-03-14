92713f99dc1ca323f6146350b46098e23d97dda1
Wahab3917 <wahab3917@gmail.com>
2024-03-15 04:02:55 +0500

    Added User Auth Functionality

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,17 @@
 const connectToMongo = require('./db');
 const express = require('express');
-// const cors = require('cors');
+const cors = require('cors');
 
 connectToMongo();
 
 const app = express();
 const port = 5000;
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 // Available Routes
-// app.use('/api/auth', require('./routes/auth'));
+app.use('/api/auth', require('./routes/auth'));
 
 
 app.listen(port, () => {
