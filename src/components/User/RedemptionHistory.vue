a66247155400afe242d9109cde7e6b5ce88cc3fa
Zainab <zainabsalman.work@gmail.com>
2024-03-15 21:06:45 +0500

    Changes in the pages

--- src/components/User/RedemptionHistory.vue
+++ src/components/User/RedemptionHistory.vue
@@ -1,10 +1,11 @@
+
 <template>
     <v-app>
         <v-app-bar> <NavBar @toggleDrawer="toggleDrawer" /> </v-app-bar>
       <SideBar :drawer="localDrawer" />
 
-      <v-main>
-        <v-container>
+      <v-main class="main">
+        <v-container class="container">
           <v-row>
             <v-col cols="12">
               <h1 class="text-style">Redemption History</h1>
@@ -42,5 +43,17 @@
     color:#4b49ac;
     font-size:30px;
   }
+  .container{
+  border-radius: 16px; /* Adjust the border-radius as per your preference */
+  background-color: white; /* Background color of the container */
+  /* Adjust padding as needed */
+  margin-top: 10px;
+  margin-bottom: 20px;
+  margin-left: 10px;
+  height: 100%;
+  max: width 100%; ;
+  }
+  .main{
+    background-color: lightgray;
+  }
   </style>
-  
--- src/components/User/userDashboard.vue
+++ src/components/User/userDashboard.vue
@@ -1,43 +1,60 @@
 <template>
-  <!-- parent listens to toggleDrawer and executes a func "toggleDrawer" -->
-  <v-app-bar> <NavBar @toggleDrawer="toggleDrawer" /> </v-app-bar>
+  <v-app>
+      <v-app-bar> <NavBar @toggleDrawer="toggleDrawer" /> </v-app-bar>
+    <SideBar :drawer="localDrawer" />
 
-  <v-main>
-    <SideBar :drawer="drawer" />
-    <v-container>
-      <redemptionPoints/> 
-    </v-container>
-  </v-main>
+    <v-main class="main">
+      <v-container class="container">
+        <v-row>
+          <v-col cols="12">
+            <h1 class="text-style">Dashboard</h1>
+            <redemptionPoints/>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
+import SideBar from '@/components/global_components/SideBar.vue';
+import NavBar from '@/components/global_components/NavBar.vue';
+import redemptionPoints from '@/components/User/redemptionPoints.vue';
 
-  import NavBar from "../global_components/NavBar.vue";
-  import SideBar from "../global_components/SideBar.vue";
-  import redemptionPoints from "./redemptionPoints.vue";
-
-  export default {
-    
-    name: "HomePage",
-    components: {
-      NavBar,
-      SideBar,
-      redemptionPoints,
-     
-    },
-
-    data() {
-      return {
-        drawer: false,
-      };
-    },
-
-    methods: {
-      toggleDrawer() {
-        this.drawer = !this.drawer;
-      },
-    },
-
-  };
-
+export default {
+  components: {
+    SideBar,
+    NavBar,
+    redemptionPoints,
+  },
+  data() {
+    return {
+      localDrawer: false
+    };
+  },
+  methods: {
+    toggleDrawer() {
+      this.localDrawer = !this.localDrawer;
+    }
+  }
+};
 </script>
+<style scoped>
+  .text-style{
+    color:#4b49ac;
+    font-size:30px;
+  }
+  .container{
+  border-radius: 16px; /* Adjust the border-radius as per your preference */
+  background-color: white; /* Background color of the container */
+  /* Adjust padding as needed */
+  margin-top: 10px;
+  margin-bottom: 20px;
+  margin-left: 10px;
+  height: 100%;
+  max: width 100%; ;
+  }
+  .main{
+    background-color: lightgray;
+  }
+  </style>
