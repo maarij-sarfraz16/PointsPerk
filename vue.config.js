f6ff8c418540db975720c98208635ba412f92f7f
Wahab3917 <wahab3917@gmail.com>
2024-04-02 01:30:28 +0500

    integrated template files

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,3 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-
-  pluginOptions: {
-    vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
-  }
-})
+// eslint-disable-next-line no-global-assign
+require = require('esm')(module);
+module.exports = require('./customize-vue-config.js');
