389493b23adca6bf8655296d022f9d0ffbf7b2dd
Wahab3917 <wahab3917@gmail.com>
2024-05-24 20:47:21 +0500

    Setup User Authentication

--- src/view/authentication/SignIn.vue
+++ src/view/authentication/SignIn.vue
@@ -1,42 +1,41 @@
 <template>
   <a-row justify="center">
     <a-col :xxl="6" :xl="12" :md="12" :sm="18">
+      
       <AuthWrapper>
         <div class="ninjadash-authentication-top">
           <h2 class="ninjadash-authentication-top__title">Sign in HexaDash</h2>
         </div>
+        
         <div class="ninjadash-authentication-content">
-          <a-form @finish="handleSubmit" :model="formState" layout="vertical">
-            <a-form-item name="username" label="Username or Email Address">
-              <a-input type="email" v-model:value="formState.email" />
+          <a-form @submit.prevent="handleSubmit" layout="vertical">
+            
+            <a-form-item name="email" label="Email">
+              <a-input type="email" v-model:value="credentials.email" placeholder="name@example.com" required/>
             </a-form-item>
-            <a-form-item name="password" initialValue="123456" label="Password">
-              <a-input
-                type="password"
-                v-model:value="formState.password"
-                placeholder="Password"
-              />
+            <a-form-item name="password" label="Password">
+              <a-input type="password" v-model:value="credentials.password" placeholder="Password" required/>
             </a-form-item>
+            
             <div class="ninjadash-auth-extra-links">
-              <a-checkbox @change="onChange">Keep me logged in</a-checkbox>
+              <a-checkbox>Keep me logged in</a-checkbox> <!-- TODO: Keep me logged in function -->
               <router-link class="forgot-pass-link" to="/auth/forgotPassword">
                 Forgot password?
               </router-link>
             </div>
+
             <a-form-item>
               <sdButton class="btn-signin" htmlType="submit" type="primary">
                 {{ isLoading ? "Loading..." : "Sign In" }}
               </sdButton>
             </a-form-item>
-            <p class="ninjadash-form-divider">
-              <span>Or</span>
-            </p>
+
+            <p class="ninjadash-form-divider"><span>Or</span></p>
+
             <ul class="ninjadash-social-login">
               <li>
                 <a class="google-social" href="#">
-                  <InlineSvg
-                    :src="require('@/static/img/icon/google-plus.svg')"
-                  ></InlineSvg>
+                  <InlineSvg :src="require('@/static/img/icon/google-plus.svg')"></InlineSvg>
                 </a>
               </li>
               <li>
@@ -44,68 +43,77 @@
                   <unicon name="facebook-f"></unicon>
                 </a>
               </li>
-              <li>
-                <a class="twitter-social" href="#">
-                  <unicon name="twitter"></unicon>
-                </a>
-              </li>
-              <li>
-                <a class="twitter-social" href="#">
-                  <unicon name="github"></unicon>
-                </a>
-              </li>
             </ul>
           </a-form>
         </div>
+
         <div class="ninjadash-authentication-bottom">
-          <p>
-            Don't have an account?<router-link to="/auth/register"
-              >Sign up</router-link
-            >
-          </p>
+          <p>Don't have an account?<router-link to="/auth/register">Sign up</router-link></p>
         </div>
       </AuthWrapper>
+
     </a-col>
   </a-row>
 </template>
+
 <script>
-import { computed, reactive, ref, defineComponent } from "vue";
-import { useStore } from "vuex";
-import { AuthWrapper } from "./style";
-import { useRouter } from "vue-router";
-import InlineSvg from "vue-inline-svg";
 
-const SignIn = defineComponent({
-  name: "SignIn",
-  components: { AuthWrapper, InlineSvg },
-  setup() {
-    const { state, dispatch } = useStore();
-    const isLoading = computed(() => state.auth.loading);
-    const checked = ref(null);
-    const router = useRouter();
+  import { computed, ref, defineComponent } from "vue";
+  import { useStore } from "vuex";
+  import { AuthWrapper } from "./style";
+  import { useRouter } from "vue-router";
+  import InlineSvg from "vue-inline-svg";
 
-    const handleSubmit = () => {
-      router.push("/");
-      dispatch("login");
-    };
-    const onChange = (checked) => {
-      checked.value = checked;
-    };
+  const SignIn = defineComponent({
+    name: "SignIn",
+    components: { AuthWrapper, InlineSvg },
+    setup() {
+      const { state, dispatch } = useStore();
+      const isLoading = computed(() => state.auth.loading);
+      const checked = ref(null);
 
-    const formState = reactive({
-      email: "example@email.com",
-      password: "1234565",
-    });
+      const host = 'http://localhost:5000';
+      const credentials = ref({ email: '', password: '' });
+      const router = useRouter();
 
-    return {
-      isLoading,
-      checked,
-      handleSubmit,
-      onChange,
-      formState,
-    };
-  },
-});
+      const handleSubmit = async () => {
+        try {
+          const response = await fetch(`${host}/api/auth/login`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(credentials.value)
+          });
 
-export default SignIn;
+          const json = await response.json();
+          if (json.success) {
+            localStorage.setItem('token', json.authToken);
+            console.log("Logged in successfully");
+            router.push('/');
+            dispatch("login");
+          } else {
+            console.log("Invalid Credentials");
+          }
+        } catch (error) {
+          console.error("Error:", error);
+        }
+      };
+
+      const onChange = (e) => {
+        credentials.value = { ...credentials.value, [e.target.name]: e.target.value };
+      };
+
+      return {
+        isLoading,
+        checked,
+        credentials,
+        handleSubmit,
+        onChange
+      };
+    },
+  });
+
+  export default SignIn;
+
 </script>
--- src/view/authentication/Signup.vue
+++ src/view/authentication/Signup.vue
@@ -1,87 +1,41 @@
 <template>
   <a-row justify="center">
     <a-col :xxl="6" :xl="12" :md="12" :sm="18">
+
       <AuthWrapper>
         <div class="ninjadash-authentication-top">
           <h2 class="ninjadash-authentication-top__title">Sign Up HexaDash</h2>
         </div>
+
         <div class="ninjadash-authentication-content">
-          <a-form
-            name="register"
-            :model="formState"
-            @finish="handleSubmit"
-            layout="vertical"
-          >
-            <a-form-item
-              label="Name"
-              name="name"
-              :rules="[
-                { required: true, message: 'Please input your Full name!' },
-              ]"
-            >
-              <a-input v-model:value="formState.name" placeholder="Full name" />
+          <a-form @submit.prevent="handleSubmit" layout="vertical">
+            
+            <a-form-item label="Name" name="name">
+              <a-input v-model:value="credentials.name" placeholder="Full name" required/>
             </a-form-item>
-            <a-form-item
-              name="username"
-              label="Username"
-              :rules="[
-                { required: true, message: 'Please input your username!' },
-              ]"
-            >
-              <a-input
-                v-model:value="formState.username"
-                placeholder="Username"
-              />
+          
+            <a-form-item name="email" label="Email Address">
+              <a-input type="email" v-model:value="credentials.email" placeholder="name@example.com" required/>
             </a-form-item>
-            <a-form-item
-              name="email"
-              label="Email Address"
-              :rules="[
-                {
-                  required: true,
-                  message: 'Please input your email!',
-                  type: 'email',
-                },
-              ]"
-            >
-              <a-input
-                type="email"
-                v-model:value="formState.email"
-                placeholder="name@example.com"
-              />
+
+            <a-form-item label="Password" name="password">
+              <a-input type="password" v-model:value="credentials.password" placeholder="Password" required/>
             </a-form-item>
-            <a-form-item
-              label="Password"
-              name="password"
-              :rules="[
-                { required: true, message: 'Please input your password!' },
-              ]"
-            >
-              <a-input
-                type="password"
-                v-model:value="formState.password"
-                placeholder="Password"
-              />
-            </a-form-item>
+
             <div class="ninjadash-auth-extra-links">
-              <a-checkbox @change="onChange">
-                Creating an account means you’re okay with our Terms of Service
-                and Privacy Policy
+              <a-checkbox required>
+                Creating an account means you’re okay with our Terms of Service and Privacy Policy
               </a-checkbox>
             </div>
+
             <a-form-item>
-              <sdButton
-                class="btn-create"
-                htmlType="submit"
-                type="primary"
-                size="lg"
-              >
+              <sdButton class="btn-create" htmlType="submit" type="primary" size="lg">
                 Create Account
               </sdButton>
             </a-form-item>
-            <p class="ninjadash-form-divider">
-              <span>Or</span>
-            </p>
+
+            <p class="ninjadash-form-divider"><span>Or</span></p>
+            
             <ul class="ninjadash-social-login">
               <li>
                 <a class="google-social" href="#">
@@ -95,61 +49,70 @@
                   <unicon name="facebook-f"></unicon>
                 </a>
               </li>
-              <li>
-                <a class="twitter-social" href="#">
-                  <unicon name="twitter"></unicon>
-                </a>
-              </li>
-              <li>
-                <a class="twitter-social" href="#">
-                  <unicon name="github"></unicon>
-                </a>
-              </li>
             </ul>
+
           </a-form>
         </div>
+
         <div class="ninjadash-authentication-bottom">
-          <p>
-            Already have an account?<router-link to="/auth/login"
-              >Sign In</router-link
-            >
-          </p>
+          <p>Already have an account?<router-link to="/auth/login">Sign In</router-link></p>
         </div>
       </AuthWrapper>
+
     </a-col>
   </a-row>
 </template>
+
 <script>
-import { AuthWrapper } from "./style";
-import { reactive, ref, defineComponent } from "vue";
-import InlineSvg from "vue-inline-svg";
 
-const SignUp = defineComponent({
-  name: "SignUp",
-  components: { AuthWrapper, InlineSvg },
-  setup() {
-    const values = ref(null);
-    const checked = ref(null);
-    const handleSubmit = (value) => {
-      values.value = value;
-    };
+  import { AuthWrapper } from "./style";
+  import { ref, defineComponent } from "vue";
+  import { useRouter } from 'vue-router';
+  import InlineSvg from "vue-inline-svg";
 
-    const onChange = (check) => {
-      checked.value = check;
-    };
-    const formState = reactive({
-      name: "",
-      username: "",
-      email: "",
-      password: "",
-    });
-    return {
-      onChange,
-      handleSubmit,
-      formState,
-    };
-  },
-});
+  const SignUp = defineComponent({
+    name: "SignUp",
+    components: { AuthWrapper, InlineSvg },
+    setup() {
+      const host = 'http://localhost:5000';
+      const credentials = ref({ name: '', email: '', password: '' });
+      const router = useRouter();
 
-export default SignUp;
+      const handleSubmit = async () => {
+        try {
+          const response = await fetch(`${host}/api/auth/createuser`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(credentials.value)
+          });
+
+          const json = await response.json();
+          if (json.success) {
+            localStorage.setItem('token', json.authToken);
+            console.log("User created successfully");
+            router.push('/');
+          } else {
+            console.log("Invalid Details");
+          }
+        } catch (error) {
+          console.error("Error:", error);
+        }
+      };
+
+      const onChange = (e) => {
+        credentials.value = { ...credentials.value, [e.target.name]: e.target.value };
+      };
+
+      return {
+        credentials,
+        handleSubmit,
+        onChange
+      };
+    }
+  });
+
+  export default SignUp;
+
 </script>
