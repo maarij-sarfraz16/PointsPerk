d2f7b6a9503b6b98c23e833b2959049562120ddd
Maarij Sarfraz <maarijsarfraz13@gmail.com>
2024-07-24 18:31:52 +0500

    added demo data in redemption history

--- src/view/pages/RedemptionHistory/redemptionHistory.vue
+++ src/view/pages/RedemptionHistory/redemptionHistory.vue
@@ -1,72 +1,70 @@
 <template>
-    <div>
-      <sdPageHeader title="Redemption History" class="ninjadash-page-header-main"></sdPageHeader>
-      <Main>
-        <DataTable
-      :filterOption="true"
-      :filterOnchange="true"
-      :tableData="tableData"
-      :columns="dataTableColumn"
-      :rowSelection="true"
-    />
-      </Main>
-    </div>
+  <div>
+    <sdPageHeader title="Redemption History" class="ninjadash-page-header-main"></sdPageHeader>
+    <Main>
+      <DataTable
+        :filterOption="true"
+        :filterOnchange="true"
+        :tableData="tableDataSource"
+        :columns="dataTableColumn"
+        :rowSelection="true"
+      />
+    </Main>
+  </div>
 </template>
-  
-  <script>
-  import { Main } from "../../styled"
-  import { defineComponent } from "vue";
-  import DataTable from "@/components/table/DataTable";
 
-  const tableDataScource = [];
+<script>
+import { Main } from "../../styled";
+import { defineComponent, ref } from "vue";
+import DataTable from "@/components/table/DataTable";
+import tableData from "@/view/pages/RedemptionHistory/table-data.json"; 
 
-  const dataTableColumn = [
-    {
-      title: 'Redemption ID',
-      dataIndex: 'id',
-      key: 'id',
-    },
-    {
-      title: 'Points Redeemed',
-      dataIndex: 'points redeemed',
-      key: 'points redeemed',
-    },
-    {
-      title: 'Coupon ID',
-      dataIndex: 'Coupon id',
-      key: 'Coupon id'
-    },
-    {
-      title: 'Coupon',
-      dataIndex: 'Coupon',
-      key: 'Coupon'
-    },
+const dataTableColumn = [
+  {
+    title: 'Redemption ID',
+    dataIndex: 'id',
+    key: 'id',
+  },
+  {
+    title: 'Points Redeemed',
+    dataIndex: 'points',
+    key: 'points',
+  },
+  {
+    title: 'Coupon ID',
+    dataIndex: 'Coupon id',
+    key: 'Coupon id',
+  },
+  {
+    title: 'Coupon',
+    dataIndex: 'Coupon',
+    key: 'Coupon',
+  },
+  {
+    title: 'Date',
+    dataIndex: 'date',
+    key: 'date',
+  },
+  {
+    title: 'Status',
+    dataIndex: 'status',
+    key: 'status',
+  },
+];
 
-    {
-      title: 'Date',
-      dataIndex: 'date',
-      key: 'date',
-    },
-    {
-      title: 'Status',
-      dataIndex: 'status',
-      key: 'status',
-    }
-    ]
+export default defineComponent({
+  name: "RedemptionHistory",
+  components: {
+    Main,
+    DataTable,
+  },
+  setup() {
+    const tableDataSource = ref(tableData);
 
-    
-
-  export default defineComponent({
-    name: "RedemptionHistory",
-    components: {
-      Main,
-      DataTable,
-    },
-    setup() {
-      return {
-        tableDataScource,
-        dataTableColumn
-      }
-    }
-  });
-  </script>
+    return {
+      tableDataSource,
+      dataTableColumn,
+    };
+  },
+});
+</script>
