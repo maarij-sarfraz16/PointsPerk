2a792274cdc85ed75b09d1b6f42705ddc7b0cdf2
Wahab3917 <wahab3917@gmail.com>
2024-08-05 01:39:00 +0500

    Remove user cover picture functionality

--- backend/models/UserProfileData.js
+++ backend/models/UserProfileData.js
@@ -10,10 +10,6 @@ const UserProfileDataSchema = new Schema({
   profilePictureUrl: {
     type: String,
     default: 'https://res.cloudinary.com/drlb3eani/image/upload/v1722537563/tnf5ax4za2k3cslb2zvq.png'
-  },
-  coverPictureUrl: {
-    type: String,
-    default: 'https://res.cloudinary.com/drlb3eani/image/upload/v1722537615/udqyaac8crfrif0ngjnp.png'
   }
 });
 
--- src/view/pages/settings/overview/CoverSection.vue
+++ src/view/pages/settings/overview/CoverSection.vue
@@ -1,38 +1,20 @@
 <template>
   <div class="cover-image">
 
-    <img style="width: 100%" :src="require('../../../../static/img/profile/cover-img.png')" alt="banner" />
-    
-    <a-upload>
-      <a to="#"> <unicon name="camera" width="16"></unicon> Change Cover </a>
-    </a-upload>
-    
+    <img style="width: 100%" :src="require('../../../../static/img/profile/cover-img.png')" alt="Banner" />
+  
   </div>
 </template>
 
 <script>
-  import axios from 'axios';
-  import { defineComponent, onMounted, ref } from 'vue';
+  import { defineComponent } from 'vue';
 
   const CoverSection = defineComponent({
     name: "CoverSection",
     components: {},
     
     setup() {
-      const host = 'http://localhost:5000';
-      const coverPicture = ref('');
-      const fileInput = ref(null);
-      const successMessage = ref('');
-      const errors = ref('');
-
-
-
-      return {
-        coverPicture,
-        fileInput,
-        successMessage,
-        errors
-      };
+      return {};
     },
   });
 
